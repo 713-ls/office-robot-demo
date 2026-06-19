df7dae43198d7531d2ff44f10d5aae327255f90f
713-ls <2067416495@qq.com>
2026-06-19 06:15:06 -0700

    chore: initialize catkin workspace and docs

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/liangsheng/office_robot_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/liangsheng/workspace/devel;/opt/ros/noetic'.split(';') if '/home/liangsheng/workspace/devel;/opt/ros/noetic' != '' else []
