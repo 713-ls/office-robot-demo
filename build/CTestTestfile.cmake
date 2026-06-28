acc628083919b1a287bd5b050573b7c6788434c7
713-ls <2067416495@qq.com>
2026-06-27 18:30:48 -0700

    sim:Before turning the navigation node parameters

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,4 +5,5 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("office_robot_navigation")
 subdirs("office_robot_sim")
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -10,6 +10,7 @@
 export CMAKE_PREFIX_PATH="/home/liangsheng/office_robot_ws/devel:$CMAKE_PREFIX_PATH"
 export LD_LIBRARY_PATH="/home/liangsheng/office_robot_ws/devel/lib:$LD_LIBRARY_PATH"
 export PATH='/opt/ros/noetic/bin:/home/liangsheng/.npm-global/bin:/home/liangsheng/.bun/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/liangsheng'
+export PKG_CONFIG_PATH="/home/liangsheng/office_robot_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/liangsheng/office_robot_ws/build'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/liangsheng/office_robot_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/liangsheng/office_robot_ws/src:$ROS_PACKAGE_PATH"
